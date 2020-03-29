a4a36f9f37a10e9a3be81ae82adeb1e34c6d3abd
hannahjayneknight <miss.knight19@imperial.ac.uk>
2020-03-29 20:54:53 +0100

    29/03/20

---
+++ chores.py
@@ -0,0 +1,12 @@
+'''
+A script that checks every minute if it is time for someone to do a chore. If it is, then 
+a message is sent to them.
+
+The chores, the time they need to be done and the person doing them is stored in an array.
+'''
+
+import numpy as np
+import time
+
+T = [['Mon', 07:30:00, 'unload the dishwasher after breakfast', 'Hannah'],
+]
