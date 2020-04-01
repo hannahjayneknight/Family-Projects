969bab92d32609b2af5181ba5c65aa358ab8e673
hannahjayneknight <miss.knight19@imperial.ac.uk>
2020-04-01 17:31:23 +0100

    01/04/20

--- chores.py
+++ chores.py
@@ -5,17 +5,22 @@ a message is sent to them.
 The chores, the time they need to be done and the person doing them is stored in an array.
 '''
 
+import time
 import datetime
+from dateutil import parser
 import numpy as np
 
 # imports the timetable
-# converts each element in the table to a string
-timetable = np.genfromtxt('/Users/hannahjayneknight/Desktop/git/personal/timetable.csv', skip_header=1, delimiter=', ', dtype=(str))
+# NB: converts each element in the table to a string
+timetable = np.genfromtxt('/Users/hannahjayneknight/Desktop/git/personal/timetable.csv', skip_header=1, delimiter=',', dtype=(str))
 
 while True:
+    # saves the last time
+    last_check = datetime.datetime.now()
+
+    time.sleep(300)
 
     # checks if the day is today
-
     # loops through the column with the weekday numbers
     for weekdays in timetable[:, 0]:
         if weekdays == str(datetime.datetime.today().weekday()):
@@ -23,25 +28,10 @@ while True:
             # continues to check if it is the correct time
 
             # loops through the column with all the times
-            for times in timetable[:, 1]:
-                # finds the time now
-                time_now = list(str(datetime.datetime.now().time()))
-                # writes the current time as a string
-                time_now_string = str(time_now[0]) + str(time_now[1]) + str(time_now[2]) + str(time_now[3]) + str(time_now[4])
-        
-                # finds the row with the time now
-                if time_now_string == str(times):
+            for i in timetable[:, 1]:
+                # checks if the time in the column is between the time of the last_check and now
+                if last_check < parser.parse(i) < datetime.datetime.now() :
                     # saves the row number with the current time as a variable
-                    row_number = times
+                    row_number = i
                     # print '[name], it is your turn to [chore]'
                     print(timetable[row_number][3] + ', it is your turn to do ' + timetable[row_number][2])
-
-'''
-Given I used a "while True" loop, I omitted the following:
-            # writes the current time +/- 1 minute as a string
-            time_plus_one_string = time_now[0] + time_now[1] + time_now[2] + time_now[3] + str(int(time_now[4])+1)
-            time_minus_one_string = time_now[0] + time_now[1] + time_now[2] + time_now[3] + str(int(time_now[4])-1)
-
-            # finds the row with the time now or +/- 1 minute 
-            if time_now_string == i or time_minus_one_string == i or time_plus_one_string == i:
-'''
